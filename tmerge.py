8827b6e80194fa53f721c1cb8ab864a41e4a2eb5
Pierre Pagnoux <pierre.pagnoux@epita.fr>
2024-04-18 01:28:52 +0200

    tmerge: check a piece hash with original file

--- tmerge.py
+++ tmerge.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 import hashlib
+import pprint
+
 import config
 from typing import OrderedDict
 
@@ -14,9 +16,12 @@ def torrent_print_metadata(torrent: OrderedDict):
     info = torrent[b"info"]
     print(f'torrent name: {info[b"name"].decode()}')
     print(f"torrent hash: {torrent_get_hash(torrent)}")
+    print(f"torrent piece length: {info[b'piece length']}")
     print(f"torrent file list:")
     for idx, file in enumerate(info[b"files"]):
         print(f"  {idx}: {file[b'path'][0].decode()} ({file[b'length']})")
+
+    print(info[b"pieces"][0:20])
 
 
 if __name__ == "__main__":
@@ -25,4 +30,21 @@ if __name__ == "__main__":
 
         torrent_print_metadata(metainfo)
 
-        config.torrent_file_to_merge_idx
+        with open(config.original_file_path, "rb") as origin:
+            assert (
+                metainfo[b"info"][b"piece length"]
+                - metainfo[b"info"][b"files"][0][b"length"]
+                == 1047898
+            )
+            origin.seek(
+                metainfo[b"info"][b"piece length"]
+                - metainfo[b"info"][b"files"][0][b"length"]
+            )
+            hash = hashlib.sha1(
+                origin.read(metainfo[b"info"][b"piece length"])
+            ).digest()
+
+            piece_sha1 = metainfo[b"info"][b"pieces"][20:40]
+
+            if hash == piece_sha1:
+                print("yes")
