16af86020058d31ba6f513401f4dda036693ce8f
Pierre Pagnoux <pierre.pagnoux@epita.fr>
2024-04-18 19:40:22 +0200

    tmerge: first version with file merged

--- tmerge.py
+++ tmerge.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 import hashlib
 import collections
+import itertools
 
 import config
 from typing import OrderedDict, BinaryIO, Iterable
@@ -10,7 +11,7 @@ import bencodepy
 File = collections.namedtuple("File", ["path", "size"])
 
 
-class Pieces(object):
+class Pieces(Iterable):
     def __init__(self, pieces_buffer):
         self._buffer = pieces_buffer
         self._piece_hash_size = 20
@@ -19,6 +20,16 @@ class Pieces(object):
         start = index * self._piece_hash_size
         end = start + self._piece_hash_size
         return self._buffer[start:end]
+
+    def __len__(self):
+        return len(self._buffer) // self._piece_hash_size
+
+    def __iter__(self):
+        for i in range(len(self)):
+            yield self[i]
+
+    def compare_with(self, other_pieces):
+        return map(lambda t: t[0] == t[1], zip(self, other_pieces))
 
 
 class Torrent(object):
@@ -36,7 +47,7 @@ class Torrent(object):
     def size(self) -> int:
         return self._info[b"length"]
 
-    def piece_length(self) -> int:
+    def piece_size(self) -> int:
         return self._info[b"piece length"]
 
     def info(self):
@@ -49,16 +60,31 @@ class Torrent(object):
     def files(self) -> Iterable[File]:
         if b"files" in self._info:
             return map(
-                lambda f: File(f[b"path"].decode(), f[b"length"]), self._info[b"files"]
+                lambda f: File(f[b"path"].decode(), f[b"length"]),
+                self._info[b"files"]
             )
 
         return [File(self.name(), self.size())]
 
 
+def get_pieces_from_file(file: BinaryIO, piece_length: int,
+                         start: int = 0) -> Pieces:
+    file.seek(start)
+    chunk = file.read(piece_length)
+    pieces: bytes = b""
+
+    while chunk:
+        pieces += hashlib.sha1(chunk).digest()
+        chunk = file.read(piece_length)
+
+    return Pieces(pieces)
+
+
 def torrent_print_metadata(torrent: Torrent):
     print(f"torrent name: {torrent.name()}")
     print(f"torrent hash: {torrent.hash().hexdigest()}")
-    print(f"torrent piece length: {torrent.piece_length()}")
+    print(f"torrent piece length: {torrent.piece_size()}")
+    print(f"torrent pieces count: {len(torrent.pieces())}")
 
     print(f"torrent file list:")
     for idx, f in enumerate(torrent.files()):
@@ -68,19 +94,65 @@ def torrent_print_metadata(torrent: Torrent):
 if __name__ == "__main__":
     with open(config.torrent_filename, "rb") as torrent:
         t = Torrent(torrent)
+        assert t.piece_size() == 262144
         torrent_print_metadata(t)
 
         with open(config.original_file_path, "rb") as origin:
-            assert t.piece_length() == 262144
-            origin.seek(0)
+            o_pieces = get_pieces_from_file(origin, t.piece_size())
 
-            hash = hashlib.sha1(origin.read(t.piece_length())).digest()
-            piece_sha1 = t.pieces()[0]
+            print(f"number of pieces torrent: {len(t.pieces())}")
+            print(f"number of pieces in origin: {len(o_pieces)}")
 
-            assert len(hash) == len(piece_sha1)
+            matches = list(t.pieces().compare_with(o_pieces))
+            print(f"{round(sum(matches) / len(matches) * 100, 2)} %")
+            print(f"{sum(matches)} / {len(matches)}")
 
-            print(f"piece hash: {hash}")
-            print(f"expected hash: {piece_sha1}")
+            with open(config.file_to_merge_path, "rb") as from_file:
+                from_pieces = get_pieces_from_file(from_file, t.piece_size())
+                print(f"number of pieces in file to merge: {len(from_pieces)}")
 
-            if hash == piece_sha1:
-                print("yes")
+                # Display stats
+
+                to_be_merged_map = list(
+                    map(
+                        lambda t: t[0] | t[1],
+                        itertools.zip_longest(
+                            t.pieces().compare_with(o_pieces),
+                            t.pieces().compare_with(from_pieces),
+                            fillvalue=False,
+                        ),
+                    )
+                )
+                print(
+                    f"chunk to merge: {sum(to_be_merged_map)} / {len(to_be_merged_map)}"
+                )
+
+                # Compute merge
+
+                merged_filepath = config.original_file_path.parent / (
+                        config.original_file_path.name + "-merged"
+                )
+                with open(merged_filepath, "wb") as out:
+                    for idx, (o_valid, f_valid) in enumerate(
+                            itertools.zip_longest(
+                                t.pieces().compare_with(o_pieces),
+                                t.pieces().compare_with(from_pieces),
+                            )
+                    ):
+                        origin.seek(idx * t.piece_size())
+                        from_file.seek(idx * t.piece_size())
+                        if f_valid and not o_valid:
+                            out.write(from_file.read(t.piece_size()))
+                        else:
+                            out.write(origin.read(t.piece_size()))
+
+                with open(merged_filepath, "rb") as out:
+
+                    out_pieces = get_pieces_from_file(out, t.piece_size())
+
+                    print(f"number of pieces torrent: {len(t.pieces())}")
+                    print(f"number of pieces in out: {len(out_pieces)}")
+
+                    matches = list(t.pieces().compare_with(out_pieces))
+                    print(f"{round(sum(matches) / len(matches) * 100, 2)} %")
+                    print(f"{sum(matches)} / {len(matches)}")
